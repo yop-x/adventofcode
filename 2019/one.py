83adbe5f79d1e11eb01fa40baaf6c2210a63975c
Yop Xue <yopxue@MacBook-Yop.local>
2025-09-16 18:09:50 +0200

    finish aoc 2019 day 1 part two

--- 2019/one.py
+++ 2019/one.py
@@ -1,7 +1,8 @@
 with open("./input_one.txt", "r") as f:
     lines = f.readlines()
     
-    
+
+# Part 1 
 
 
 
@@ -9,13 +10,63 @@ modules = []
 for line in lines:
     modules.append(line)
     
+
+
+
+def get_fuel(number):
+    fuel = int(number)//3 - 2 
+    return fuel
     
-results = [int(float(int(number)/3))-2 for number in modules]
+    
+    
+    
+final = 0 
+for number in modules:
+    final += get_fuel(number)
+    
+    
+print(f'The answer to part one is {final}.')
+    
+    
+
+# Part 2 
+"""
+
+A module of mass 14 requires 2 fuel. 
+This fuel requires no further fuel (2 divided by 3 and rounded down is 0, which would call for a negative fuel), 
+so the total fuel required is still just 2.
+
+At first, a module of mass 1969 requires 654 fuel. 
+Then, this fuel requires 216 more fuel (654 / 3 - 2). 
+216 then requires 70 more fuel, which requires 21 fuel, which requires 5 fuel, which requires no further fuel. 
+So, the total fuel required for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.
+The fuel required by a module of mass 100756 and its fuel is: 
+33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346. 
+"""
+
+
+#take one number and return the total sum 
+
+
+1969 
+
+
+
+def recursive_fuel(number):
+    
+    new_number = get_fuel(int(number))
+    
+    
+    if new_number <= 0:
+        return 0 
+        
+    else:
+        return new_number + recursive_fuel(new_number)
+    
+
 
 final = 0 
-for number in results:
-    final += number 
+for num in modules:
+    final += recursive_fuel(num)
     
-    
-print(final)
-    
+print(f'the answer to part two is {final}.')
