2b0982bb56051155acce5a9885b7d7f541164cd7
yop-x <yopxue@gmail.com>
2025-09-22 21:13:12 +0200

    day four part 2

--- 2019/four.py
+++ 2019/four.py
@@ -1,4 +1,5 @@
 # day four 
+import re 
 
 # part one 
 
@@ -17,4 +18,20 @@ for number in passwords:
     if str(number)[5] >= str(number)[4] >= str(number)[3] >= str(number)[2] >= str(number)[1] >= str(number)[0]:
         passwords_updated.append(number)
         
-print(len(set(passwords_updated)))
+print(f'Part one answer: {len(set(passwords_updated))}')
+
+
+# part two 
+part_two = []
+for number in set(passwords_updated):
+    score = 0 
+    for i in range(0,4):
+        if str(number)[i] == str(number)[i+1] and str(number)[i] == str(number)[i+2]:
+            score += 1 
+    if score == 4 or score == 2 or score == 0:
+        part_two.append(number)
+            
+            
+print(len(set(part_two))) 
+
+            
