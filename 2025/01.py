70f82b7730d27386a925236bb3bb4ca65ca17f71
yop-x <yopxue@gmail.com>
2025-12-01 18:05:47 +0100

    modified part two not successful

--- 2025/01.py
+++ 2025/01.py
@@ -33,4 +33,59 @@ print(f"The answer for part one is {real_code}.")
 
 
 # part two 
+code = 50 
+real_code = 0 
+# x = ['L68',
+#     'L30',
+#     'R48',
+#     'L5',
+#     'R60',
+#     'L55',
+#     'L1',
+#     'L99',
+#     'R14',
+#     'L82']
+
+for step in x:
+    if step[0] == 'L':
+        
+        code = code - int(step[1:])
+        if code < 0 and (abs(code) // 100 == 0):
+            real_code += 1 
+            
+        q = abs(code) // 100 
+        real_code = real_code + q 
+        
+        code = code % 100
+        
+        if code < 0: 
+            code = 100 - abs(code)
+            
+            
+        elif code == 0:
+            code = code
+            real_code += 1 
+            
+            
+    if step[0] == 'R': 
+        code = code + int(step[1:])
+        if code > 99 and (abs(code) // 100 == 0):
+            real_code += 1 
+            
+        q = abs(code) // 100 
+        real_code = real_code + q 
+        code = code % 100
+
+
+        if code < 0:
+            code = 100 - abs(code)
+            
+            
+            
+        elif code == 0:           
+            real_code += 1 
+
+        
+            
+print(f"The answer for part two is {real_code}")
 
